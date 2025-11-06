39dd7cb57f2e582521f995b25f757a703f589f07
teHaginoya <it.te.haginoya@iftc.co.jp>
2025-11-06 16:53:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ st.markdown("""
         padding-bottom: 1rem;
         padding-left: 0.5rem;
         padding-right: 0.5rem;
-        max-width: 20%;
+        max-width: 200px;
     }
     
     /* カラムコンテナの設定 - 5つ並ぶように */
